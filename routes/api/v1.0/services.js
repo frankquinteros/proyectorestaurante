3107f34d94dc1341bc062bed4e53aea5e5c12ae7
marlene salazar <marlenesalazar1995@gmail.com>
2018-12-01 20:13:41 -0400

    añadiendo apirestaurante

--- routes/api/v1.0/services.js
+++ routes/api/v1.0/services.js
@@ -3,14 +3,15 @@ var multer = require('multer');
 var router = express.Router();
 var fs = require('fs');
 //var _ = require("underscore");
-var Home = require("../../../database/collections/homes");
-var Img = require("../../../database/collections/img");
+var RESTAURANT = require("../../../database/collections/restaurant");
+var MENUS = require("../../../database/collections/menus");
+var CLIENT= require("../../../database/collections/clients");
 
 var jwt = require("jsonwebtoken");
 
 
 var storage = multer.diskStorage({
-  destination: "./public/avatars",
+  destination: "./public/restaurants",
   filename: function (req, file, cb) {
     console.log("-------------------------");
     console.log(file);
@@ -21,34 +22,7 @@ var upload = multer({
   storage: storage
 }).single("img");;
 
-/*
-Login USER
-*/
-router.post("/login", (req, res, next) => {
-  var username = req.body.username;
-  var password = req.body.password;
-  var result = Home.findOne({name: username,password: password}).exec((err, doc) => {
-    if (err) {
-      res.status(200).json({
-        msn : "No se puede concretar con la peticion "
-      });
-      return;
-    }
-    if (doc) {
-      //res.status(200).json(doc);
-      jwt.sign({name: doc.name, password: doc.password}, "secretkey123", (err, token) => {
-          console.log(err);
-          res.status(200).json({
-            token : token
-          });
-      })
-    } else {
-      res.status(200).json({
-        msn : "El usuario no existe ne la base de datos"
-      });
-    }
-  });
-});
+
 //Middelware
 function verifytoken (req, res, next) {
   //Recuperar el header
@@ -59,7 +33,7 @@ function verifytoken (req, res, next) {
       })
   } else {
       req.token = header.split(" ")[1];
-      jwt.verify(req.token, "secretkey123", (err, authData) => {
+      jwt.verify(req.token, "seponeunallavesecreta", (err, authData) => {
         if (err) {
           res.status(403).json({
             msn: "No autotizado"
@@ -293,4 +267,154 @@ router.put(/home\/[a-z0-9]{1,}$/, verifytoken,(req, res) => {
       return;
   });
 });
+
+//APIRESTAURANTE
+/*
+Login USER
+*/
+router.post("/login", (req, res, next) => {
+  var email = req.body.email;
+  var password = req.body.password;
+  var result = CLIENT.findOne({email: email,password: password}).exec((err, doc) => {
+    if (err) {
+      res.status(200).json({
+        msn : "No se puede concretar con la peticion "
+      });
+      return;
+    }
+    if (doc) {
+      //res.status(200).json(doc);
+      jwt.sign({name: doc.email, password: doc.password}, "seponeunallavesecreta", (err, token) => {
+          console.log(err);
+          res.status(200).json({
+            token : token
+          });
+      })
+    } else {
+      res.status(200).json({
+        msn : "El usuario no existe en la base de datos"
+      });
+    }
+  });
+});
+//registrar cliente
+router.post("/clients", (req, res) => {
+  var client = req.body;
+  //validar datos
+  client["registerdate"] = new Date();
+  var cli = new CLIENT(client);
+  cli.save().then((docs) => {
+   res.status(200).json(docs); 
+  });
+});
+//registrar restaurante
+router.post("/restaurant",verifytoken ,(req, res) =>{
+var data = req.body;
+data["registerdate"] = new Date();
+//realizar validacion y seguridad
+var newrestaurant = new RESTAURANT(data);
+  newrestaurant.save().then( (rr) => {
+    //content-type
+    res.status(200).json({
+      "id" : rr._id,
+      "msn" : "restaurante agragado con exito "
+    });
+  });
+});
+//5bfdd0abeaf9e7120e9df600
+router.get("/restaurant",verifytoken ,(req, res) => {
+  var skip = 0;
+  var limit = 10;
+  if (req.query.skip != null) {
+    skip = req.query.skip;
+  }
+  if (req.query.limit != null) {
+    limit = req.query.limit;
+  }
+RESTAURANT.find({}).skip(skip).limit(limit).exec((err, docs) => {
+if (err) {
+          res.status(500).json({
+            "msn" : "Error en la base de datos"
+          });
+          return;
+        }
+res.status(200).json(docs);
+});
+});
+
+//coleccion de datos
+router.patch("/restaurant",verifytoken ,(req, res) =>{
+  var params = req.body;
+  var id = req.query.id;
+
+  var keys = Object.keys(params);
+  var updatekeys = ["name", "nit", "property", "street", "phone", "Log", "Lat", "logo", "picture"];
+  var newkeys = [];
+  var values = [];
+  //seguridad
+  for (var i = 0; i < updatekeys.length; i++){
+    var index = keys.indexOf(updatekeys[i]);
+    if (index != -1) {
+      newkeys.push(keys[index]);
+      values.push(params[keys[index]]);
+    }
+  }
+  var objupdate = {}
+  for (var i = 0; i < newkeys.length; i++) {
+      objupdate[newkeys[i]] = values[i];
+  }
+  console.log(objupdate);
+  RESTAURANT.findOneAndUpdate({_id: id}, objupdate, (err, docs) => {
+    if (err) {
+      res.status(500)-json({
+        msn: "existe un error en la base de datos"
+      });
+      return;
+    }
+    var id = docs._id
+    res.status(200).json({
+      mns: id
+    })
+  });
+
+});
+
+router.post("/uploadrestaurant",verifytoken , (req, res) => {
+  var params = req.query;
+  var id = params.id;
+  var SUPERES = res;
+  RESTAURANT.findOne({_id: id}).exec((err, docs) => {
+    if (err) {
+      res.status(501).json({
+        "msn" : "problemas con la base de datos"
+      });
+      return;
+    }
+    if (docs != undefined){
+      upload(req, res, (err) => {
+        if (err) {
+          res.status(500).json({
+            "msn" : "Error al subir la imagen"
+          });
+          return;
+        }
+        var url = req.file.path.replace(/public/g, "");
+       
+        RESTAURANT.update({_id: id}, {$set:{picture:url}}, (err, docs) => {
+          if (err) {
+            res.status(200).json({
+              "mns" : err
+            });
+            return;
+          }
+          res.status(200).json(docs);
+        });
+      });
+    }
+  });
+});
+//"public/restaurants/IMG_1543376089904.jpg"
+
 module.exports = router;
+
+
